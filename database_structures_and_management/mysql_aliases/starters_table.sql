192e57bde5c755c901e55c333c8d9611f4dfa365
Tolani99 <tolaniadisa999@gmail.com>
2023-04-22 20:25:07 +0100

    Table Starters

---
+++ database_structures_and_management/mysql_aliases/starters_table.sql
@@ -0,0 +1,2 @@
+create table Starters(StarterName varchar(50), Cost decimal(3,2));
+insert into Starters(StarterName, Cost) values('Olives', 4.75), ('Flatbread', 5.00), ('Minestrone', 8.50), ('Tomato bread', 8.50), ('Falafel', 6.75), ('Hummus', 5.00);
