b5cdae8e11d7cedc27677b71f5321f142a77b5cf
Tolani99 <tolaniadisa999@gmail.com>
2023-04-22 20:02:58 +0100

    Use of aliases to change column names

---
+++ database_structures_and_management/mysql_aliases/use_of_aliases_to_change_column_name.sql
@@ -0,0 +1,1 @@
+SELECT OrderID, Date_food_order_placed_with_supplier AS "Order Placed Date", Date_food_order_received_from_supplier AS "Order Received Date", Order_status from Food_Orders_Delivery_status; 
