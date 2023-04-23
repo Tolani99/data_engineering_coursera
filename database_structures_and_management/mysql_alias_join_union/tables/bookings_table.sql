abd393549bfb777acd17c0fd1ad19abf271e6aa1
Tolani99 <tolaniadisa999@gmail.com>
2023-04-23 14:04:32 +0100

    A README file explaining the purpose of this directory

---
+++ database_structures_and_management/mysql_alias_join_union/tables/bookings_table.sql
@@ -0,0 +1,2 @@
+CREATE TABLE bookings_table(BookingID INT, BookingDate DATE, TableNumber INT, NumberOfGuests INT, CustomerID INT);
+INSERT INTO bookings_table(BookingID, BookingDate, TableNumber, NumberOfGuests, CustomerID) VALUES(10, '2021-11-11', 7, 5 ,1), (11, '2021-11-10', 5, 2, 2), (12, '2021-11-10', 3, 2, 4);
