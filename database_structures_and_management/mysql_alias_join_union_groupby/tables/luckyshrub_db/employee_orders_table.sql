cab7ba92d77bf7b1875baa99d97e2094dc76b0aa
Tolani99 <tolaniadisa999@gmail.com>
2023-04-24 11:45:16 +0100

    Employees order table

---
+++ database_structures_and_management/mysql_alias_join_union_groupby/tables/luckyshrub_db/employee_orders_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee_orders (
+ OrderID int NOT NULL,
+ EmployeeID int NOT NULL,
+ Status VARCHAR(150),
+ HandlingCost int DEFAULT NULL,
+ PRIMARY KEY (EmployeeID,OrderID),
+ FOREIGN KEY (EmployeeID) REFERENCES employees(EmployeeID),
+ FOREIGN KEY (OrderID) REFERENCES orders(OrderID)
+);
