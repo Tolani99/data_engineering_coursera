987c8965b3fcf7132a86491279d16b0c970156ef
Tolani99 <tolaniadisa999@gmail.com>
2023-04-24 11:44:50 +0100

    Order table

---
+++ database_structures_and_management/mysql_alias_join_union_groupby/tables/luckyshrub_db/orders.sql
@@ -0,0 +1,8 @@
+CREATE TABLE orders (
+  OrderID int NOT NULL,
+  Department varchar(100) DEFAULT NULL,
+  OrderDate date DEFAULT NULL,
+  OrderQty int DEFAULT NULL,
+  OrderTotal int DEFAULT NULL,
+  PRIMARY KEY (OrderID)
+);
