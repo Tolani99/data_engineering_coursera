5b2db00ca616e1fe6404ea2dda0d406cbe5fb42e
Tolani99 <tolaniadisa999@gmail.com>
2023-04-24 11:44:26 +0100

    Employees table

---
+++ database_structures_and_management/mysql_alias_join_union_groupby/tables/luckyshrub_db/employees.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+  EmployeeID int NOT NULL,
+  EmployeeName varchar(150) DEFAULT NULL,
+  Department varchar(150) DEFAULT NULL,
+  ContactNo varchar(12) DEFAULT NULL,
+  Email varchar(100) DEFAULT NULL,
+  AnnualSalary int DEFAULT NULL,
+  PRIMARY KEY (EmployeeID)
+);
