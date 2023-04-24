068dc3069eabf63d35d9fd5e95123dbe4aeb31b8
Tolani99 <tolaniadisa999@gmail.com>
2023-04-24 14:46:57 +0100

    EmployeeContactInfo.sql

---
+++ database_structures_and_management/mysql_alias_join_union_groupby/tables/luckyshrub_db/EmployeeContactInfo.sql
@@ -0,0 +1,2 @@
+CREATE TABlE EmployeeContactInfo(EmployeeID int not null primary key, ContactNumber int, Email varchar(255));
+INSERT INTO EmployeeContactInfo(EmployeeID, ContactNumber, Email) VALUES(1, 351478025, 'seamus.h@luckyshrub.com');
