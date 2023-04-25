dd406b5896bdca9ab7bb7bc0a7df8c5e6e691fe9
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 14:16:15 +0100

    Using alter to modify Machinery table's datatype

---
+++ database_structures_and_management/alter/work_on_Lucky_Shrub/using_alter_to_modify.sql
@@ -0,0 +1,1 @@
+alter table Machinery modify EmployeeID varchar(10) not null primary key, modify FullName varchar(100) not null, modify County varchar(100) not null, modify PhoneNumber int not null unique;
