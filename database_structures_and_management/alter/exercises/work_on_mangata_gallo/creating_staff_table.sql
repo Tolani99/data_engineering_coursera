22251431db54de586ab198daea0ec58bf233ec3e
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 18:04:08 +0100

    creating staff table

---
+++ database_structures_and_management/alter/exercises/work_on_mangata_gallo/creating_staff_table.sql
@@ -0,0 +1,14 @@
+/*
+Task 1
+
+Write a SQL statement that creates the Staff table with the following columns.
+
+StaffID: INT
+
+FullName: VARCHAR(100)
+
+PhoneNumber: VARCHAR(10)
+
+The table structure should be the same as the following screenshot (assuming that you have created and populated the table correctly.)
+*/
+create database Mangata_gallo; use Mangata_gallo; create table Staff(StaffID int, FullName varchar(100), PhoneNumber varchar(10)); select * from Staff;
