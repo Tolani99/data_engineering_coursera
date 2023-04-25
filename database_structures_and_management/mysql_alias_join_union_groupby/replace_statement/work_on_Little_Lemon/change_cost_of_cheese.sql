e99b28280196938c9b410bf5f2e1a6361a48fe4f
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 11:38:27 +0100

    Using change of cheese cost

---
+++ database_structures_and_management/mysql_alias_join_union_groupby/replace_statement/work_on_Little_Lemon/change_cost_of_cheese.sql
@@ -0,0 +1,4 @@
+/*
+Task 2: Use the REPLACE statement to change the cost of the Cheese bread from $9.50 to $9.75.
+*/
+REPLACE INTO Starter SET StarterName = 'Cheesebread', Cost = 9.75, StarterType = 'Indian'; 
