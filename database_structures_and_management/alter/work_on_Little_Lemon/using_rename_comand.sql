cc934b717492ec5e139d484876fcbd7f4476861a
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 21:23:53 +0100

    Renaming command with alter statement

---
+++ database_structures_and_management/alter/work_on_Little_Lemon/using_rename_comand.sql
@@ -0,0 +1,4 @@
+/*
+Task 6: Use the RENAME command with ALTER statement to change the table name from OrderStatus to OrderDeliveryStatus.
+*/
+alter table food_delivery_status rename to OrderDeliveryStatus;
