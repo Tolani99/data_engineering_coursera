b6df6b2f5c1000195bd5cc47b2af3a77529ffe78
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 18:22:25 +0100

    Dropping a column from the table

---
+++ database_structures_and_management/alter/exercises/work_on_mangata_gallo/to_drop_phone_number_from_columns.sql
@@ -0,0 +1,6 @@
+/*
+Write a SQL statement that drops the Phone Number column from the 'Staff' table.
+
+The expected structure of the table after removing the Phone Number column should be the same as the following screenshot (assuming that you have created and populated the tables correctly.)
+*/
+alter table Staff drop Role;
