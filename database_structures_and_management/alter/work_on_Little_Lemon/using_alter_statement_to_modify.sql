d3444bdc2da6580abdb1d49a747c161866a6322b
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 20:33:52 +0100

    Using alter statement to modify

---
+++ database_structures_and_management/alter/work_on_Little_Lemon/using_alter_statement_to_modify.sql
@@ -0,0 +1,3 @@
+/*
+Task 1: Use the ALTER TABLE statement with MODIFY command to make the OrderID the primary key of the 'FoodOrders' table. */
+alter table FoodOrders modify OrderID int primary key;
