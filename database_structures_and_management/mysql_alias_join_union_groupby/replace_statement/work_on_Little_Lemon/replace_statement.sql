7f31c543ba7e2a269539c58054be41bc99d4d1b9
Tolani99 <tolaniadisa999@gmail.com>
2023-04-25 11:35:45 +0100

    Use of replace statement in little lemon table

---
+++ database_structures_and_management/mysql_alias_join_union_groupby/replace_statement/work_on_Little_Lemon/replace_statement.sql
@@ -0,0 +1,10 @@
+/*
+Task 1: Use the REPLACE statement to insert a new data record with the following details. 
+
+The ‘StarterName’ = Cheese bread
+
+The ‘Cost’ = 9.50 and 
+
+The ‘StarterType’ = Indian
+*/
+REPLACE INTO Starter(StarterName, Cost, StarterType) VALUES('Cheesebread', 9.5, 'Indian'); 
