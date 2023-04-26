33a7467bbf0af957ca64fe7dc5296789679de367
Tolani99 <tolaniadisa999@gmail.com>
2023-04-26 12:38:21 +0100

    Finding the menu items cost

--- database_structures_and_management/subqueries/work_with_littlelemon_db/task_3_finding_menu_items_cost.sql
+++ database_structures_and_management/subqueries/work_with_littlelemon_db/task_3_finding_menu_items_cost.sql
@@ -3,4 +3,4 @@ Task 3: Write a SQL SELECT query to find the menu items that costs the same as t
 
 The expected output result should be the same as the following screenshot (assuming that you have created and populated the orders tables correctly). 
 */
-
+SELECT * FROM MenuItems WHERE Price = (SELECT Price FROM Menus, MenuItems WHERE Menus.ItemID = MenuItems.ItemID AND MenuItems.Type = 'Starters' AND Cuisine = 'Italian'); 
