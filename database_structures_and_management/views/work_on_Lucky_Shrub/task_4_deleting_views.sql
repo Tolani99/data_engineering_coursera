da52e25bf34cd1fe5a2f1072d0e27b0b89d27877
Tolani99 <tolaniadisa999@gmail.com>
2023-04-26 15:13:59 +0100

    Deleting orders view

---
+++ database_structures_and_management/views/work_on_Lucky_Shrub/task_4_deleting_views.sql
@@ -0,0 +1,6 @@
+/*
+Task 4: Write a SQL statement to delete the Orders virtual table.
+
+Once you have completed these tasks, check and compare your answers with the solutions in the reading solution section.
+*/
+drop view ClientsOrdersView;
