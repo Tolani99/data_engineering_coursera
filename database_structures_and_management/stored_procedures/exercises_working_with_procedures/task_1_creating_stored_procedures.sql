ec768194e61fb78ae9596ebae1943f3cb50cecd5
Tolani99 <tolaniadisa999@gmail.com>
2023-04-29 11:17:05 +0100

    Creating stored procedures

---
+++ database_structures_and_management/stored_procedures/exercises_working_with_procedures/task_1_creating_stored_procedures.sql
@@ -0,0 +1,4 @@
+/*
+Task 1: Write a SQL statement that creates a stored procedure called 'GetOrdersData' which retrieves all data from the Orders table.
+*/
+create procedure GetOrdersDate() select * from Orders;
