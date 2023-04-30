0e94055b40723a9f5ea2a31e02d084f449b48a75
Tolani99 <tolaniadisa999@gmail.com>
2023-04-30 20:20:36 +0100

    Adding columns in a table

---
+++ database_structures_and_management/database_project/altering_table_structure.sql
@@ -0,0 +1,8 @@
+/*
+Task 6: Alter table structure
+
+Create a SQL statement that adds a new column called 'Ingredients' to the Courses table.
+
+Ingredients: VARCHAR(255)
+*/
+alter table Courses add Ingredients varchar(255);
