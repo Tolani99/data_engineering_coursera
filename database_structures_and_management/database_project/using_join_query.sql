7f15f29cd3363867542444bf097b6226ff9bd64d
Tolani99 <tolaniadisa999@gmail.com>
2023-04-30 19:42:08 +0100

    Using join query in sql

---
+++ database_structures_and_management/database_project/using_join_query.sql
@@ -0,0 +1,6 @@
+/*
+Task 2: Create a JOIN query.
+
+Create a JOIN SQL statement on the Customers and Bookings tables. The statement must print the customers full names and related bookings IDs from the date 2021-11-11.
+*/
+select Customers.FullName, Bookings.BookingID from Customers right join Bookings on Customers.CustomerID = Bookings.CustomerID where BookingDate = '2021-11-11';
