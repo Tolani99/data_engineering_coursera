86cf5c54ff0adf7412b28094d70e5c7d45aafedf
Tolani99 <tolaniadisa999@gmail.com>
2023-05-02 09:19:36 +0100

    Creating if else composed function

--- advanced_mysql/developing_user_defined_functions/project_2_creating_if_else_composed_function.sql
+++ advanced_mysql/developing_user_defined_functions/project_2_creating_if_else_composed_function.sql
@@ -2,3 +2,5 @@ DELIMETER//
 create function GetTotalCost(Cost decimal(5, 2)) returns decimal(5, 2) deterministic begin if (Cost >= 100 and Cost < 500) then set Cost = Cost - (Cost * 0.1); elseif (Cost >= 500) then set Cost = Cost - (Cost * 0.2); end if; return
 (Cost);
 END//
+DELIMETER ;
+SELECT GetTotalCost(500);
