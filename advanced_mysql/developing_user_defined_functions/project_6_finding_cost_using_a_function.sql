fe501119311ec7a36bc912060c87b792500134c0
Tolani99 <tolaniadisa999@gmail.com>
2023-05-03 13:24:54 +0100

    Using stored function to find cost given order ID

--- advanced_mysql/developing_user_defined_functions/project_6_finding_cost_using_a_function.sql
+++ advanced_mysql/developing_user_defined_functions/project_6_finding_cost_using_a_function.sql
@@ -1,2 +1,8 @@
+/**
+Task 1:
+
+Create a SQL function that prints the cost value of a specific order based on the user input of the OrderID. The expected output result should be the same as the result in the screenshot below when you call the function with an OrderID of 5.
+**/
+
 create function FindCost(Id_no int) returns decimal(5, 2) deterministic return (select Cost from Orders where OrderID = Id_no);
 select FindCost(5);
