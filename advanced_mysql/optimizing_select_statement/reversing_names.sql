1797b66c5260e5a2d4cc2aaa52e9f21c21dffa7f
Tolani99 <tolaniadisa999@gmail.com>
2023-05-08 13:33:39 +0100

    Reversing a string in mysql

---
+++ advanced_mysql/optimizing_select_statement/reversing_names.sql
@@ -0,0 +1,4 @@
+alter table ORDERS add column ExpectedDeliveryDate DATE;
+select * from Clients where FullName like '%Ito';
+alter table Clients add column ReverseFullName varchar(200);
+update Clients set ReverseFullName = concat(substring_index(FullName, ' ', -1), ' ', substring_index(FullName, ' ', 1));
