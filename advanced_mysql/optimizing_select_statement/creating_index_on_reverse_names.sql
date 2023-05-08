a89b6445ce77c598147f0f14e018ed0bfb7c639c
Tolani99 <tolaniadisa999@gmail.com>
2023-05-08 13:40:59 +0100

    Creating an index on reverse names

--- advanced_mysql/optimizing_select_statement/creating_index_on_reverse_names.sql
+++ advanced_mysql/optimizing_select_statement/creating_index_on_reverse_names.sql
@@ -1,3 +1,10 @@
+/*
+The SQL statement create index IdxReverseFullName on Clients(ReverseFullName); creates an index named IdxReverseFullName on the ReverseFullName column of the Clients table.
+
+This index will improve the performance of queries that use the ReverseFullName column as a search or sort criterion. When an index is created on a column, the database management system creates a separate data structure that contains a sorted list of the values in that column, along with pointers to the corresponding rows in the table. This allows the system to quickly locate the rows that match a given search condition or sort order without having to scan the entire table.
+
+Note that creating an index can improve performance for some types of queries, but it can also slow down data modification operations such as inserts, updates, and deletes. This is because the index must be updated along with the table data whenever a modification is made. Therefore, it's important to carefully consider the trade-offs between query performance and data modification performance when deciding whether to create an index.
+*/
 alter table ORDERS add column ExpectedDeliveryDate DATE;
 select * from Clients where FullName like '%Ito';
 alter table Clients add column ReverseFullName varchar(200);
