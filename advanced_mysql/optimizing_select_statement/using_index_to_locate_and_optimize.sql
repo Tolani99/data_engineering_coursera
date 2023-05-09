dd2e3c5a750edb01a7b872ae05d850c7566ac510
Tolani99 <tolaniadisa999@gmail.com>
2023-05-09 12:46:51 +0100

    Creating an index to locate and optimize a database

--- advanced_mysql/optimizing_select_statement/using_index_to_locate_and_optimize.sql
+++ advanced_mysql/optimizing_select_statement/using_index_to_locate_and_optimize.sql
@@ -1,3 +1,14 @@
+/*
+The first command "select * from Clients;" selects all data from the "Clients" table.
+
+The second command "select ContactNumber from Clients where FullName = 'Jane Delgado';" selects the "ContactNumber" column from the "Clients" table where the "FullName" column is equal to 'Jane Delgado'.
+
+The third command "create index IdxFullName on Clients(FullName);" creates an index on the "FullName" column in the "Clients" table. This index will improve the performance of queries that use the "FullName" column as a filter, such as the query in the second command.
+
+By creating an index on the "FullName" column, the database can quickly locate the rows that match the filter condition, instead of scanning the entire table. This can significantly reduce the time it takes to execute queries that use the "FullName" column as a filter.
+
+It is important to note that creating an index can have an impact on the performance of insert, update, and delete operations on the table, as the index needs to be updated to reflect any changes to the data. Therefore, it is important to carefully consider the trade-offs between query performance and data modification performance when deciding whether to create an index.
+*/
 select * from Clients;
 explain select ContactNumber from Clients where FullName = 'Jane Delgado';
 create index IdxFullName on Clients(FullName);
