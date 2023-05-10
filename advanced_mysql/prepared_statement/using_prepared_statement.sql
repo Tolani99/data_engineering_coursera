cf13c029a8e6c2d30ba5c8e2e3eaec5ce262e75b
Tolani99 <tolaniadisa999@gmail.com>
2023-05-10 08:27:04 +0100

    Using a prepared statement

--- advanced_mysql/prepared_statement/using_prepared_statement.sql
+++ advanced_mysql/prepared_statement/using_prepared_statement.sql
@@ -1,3 +1,17 @@
+/*
+The given code is a SQL query that uses a prepared statement and executes it with a parameter value.
+
+Here's how it works:
+
+The first line declares a prepared statement called "GetOrderStatement". It is a SELECT statement that retrieves data from the "Orders" table based on the "OrderID" parameter. The statement includes placeholders (denoted by "?") for the parameter value that will be passed in later.
+
+The second line sets a value of 10 to the variable "@order_id". This will be used as the parameter value for the prepared statement.
+
+The third line executes the prepared statement using the parameter value "@order_id". The "EXECUTE" keyword is used to execute the statement, and the "USING" keyword is used to pass in the parameter value. When the statement is executed, the parameter value replaces the placeholder in the prepared statement, and the resulting SQL statement is executed.
+
+In summary, this code prepares a SQL statement that selects a specific order from the "Orders" table based on an OrderID parameter, sets a value for the OrderID parameter, and then executes the statement using the specified parameter value to retrieve the order information.
+*/
+Prepare the statement using the PREPARE statement:
 prepare GetOrderStatement from 'select ClientID, ProductID, Quantity, Cost from Orders where OrderID = ?';
 SET @order_id = 10;
 EXECUTE GetOrderStatement USING @order_id;
