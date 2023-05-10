3a8f5b25e13a9d9c77a715cb30e740b0c1a53d35
Tolani99 <tolaniadisa999@gmail.com>
2023-05-10 23:22:00 +0100

    Using full outer join in mysql

--- advanced_mysql/data_analysis_using_sql/using_full_outer_join.sql
+++ advanced_mysql/data_analysis_using_sql/using_full_outer_join.sql
@@ -4,3 +4,6 @@ This SQL query retrieves a combined list of all clients and their corresponding 
 The query uses a UNION operator to combine the results of two SELECT statements. The first SELECT statement retrieves records from the Clients table using a LEFT JOIN on the Orders table, while the second SELECT statement retrieves records from the Orders table using a RIGHT JOIN on the Clients table. This ensures that all records from both tables are included in the result set.
 */
 select Clients.ClientID, Clients.FullName, Clients.ContactNumber, Orders.OrderID, Orders.Cost, Orders.Date from Clients left join Orders on Clients.ClientID = Orders.ClientID union select Clients.ClientID, Clients.FullName, Clients.ContactNumber, Orders.OrderID, Orders.Cost, Orders.Date from Clients RIGHT JOIN Orders on Clients.ClientID = Orders.ClientID;
+/*
+Note that the UNION operator removes duplicates from the result set. If you want to include duplicates, you can use UNION ALL instead.
+*/
