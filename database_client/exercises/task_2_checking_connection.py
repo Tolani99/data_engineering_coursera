d63cc7af115b64b92c761ebbab3c7ba4f2e41ce5
Tolani99 <tolaniadisa999@gmail.com>
2023-06-11 14:29:05 +0100

    Creating a menu item table in mysql

--- database_client/exercises/task_2_checking_connection.py
+++ database_client/exercises/task_2_checking_connection.py
@@ -2,4 +2,4 @@ import mysql.connector as connector
 connection = connector.connect(user="root", password="tolamath")
 cursor = connection.cursor()
 cursor.execute("use little_lemon")
-print(connection.database)
+#print(connection.database)
