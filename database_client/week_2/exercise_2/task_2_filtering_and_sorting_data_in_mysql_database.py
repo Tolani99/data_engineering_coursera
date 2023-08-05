d0f0f63fc36bfe470dda1ba6dc2baa2d133d3d05
Tolani99 <tolaniadisa999@gmail.com>
2023-08-05 21:36:37 +0100

    Filtering and sorting records of all guest who qualify for coupons

--- database_client/week_2/exercise_2/task_2_filtering_and_sorting_data_in_mysql_database.py
+++ database_client/week_2/exercise_2/task_2_filtering_and_sorting_data_in_mysql_database.py
@@ -1,3 +1,9 @@
+"""
+Task 2:
+
+Little Lemon want to send a coupon to all guests who spent more than $40 at the restaurant today. Help Little Lemon to filter and sort the records of all guests who qualify for coupons.
+"""
+
 from task_1_filtering_and_sorting_data_in_mysql_database import cursor
 
 # Check the constituent of the table
