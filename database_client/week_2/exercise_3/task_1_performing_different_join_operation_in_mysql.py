a85b2d6f687bae8a8954ef8c8dabbcf7fe878b2f
Tolani99 <tolaniadisa999@gmail.com>
2023-08-06 23:17:57 +0100

    Joining tables to extract information for each items on the Menu

---
+++ database_client/week_2/exercise_3/task_1_performing_different_join_operation_in_mysql.py
@@ -0,0 +1,50 @@
+"""
+Task 1:
+
+Little Lemon needs the following information for each of the items on their menu:
+
+The name of each item in the menu,
+
+Each menu item’s type,
+
+Each menu item’s cuisine,
+
+and the price of each item in the menu.
+"""
+
+import mysql.connector as connector
+connection = connector.connect(user = "root", password = "tolamath")
+cursor = connection.cursor()
+
+# Use little_lemon
+cursor.execute("USE little_lemon")
+
+# The join query statement
+join_query = """
+SELECT 
+    MenuItems.Name AS Item_Name,
+    MenuItems.Type AS Item_Type,
+    MenuItems.Price AS Item_Price,
+    Menus.Cuisine AS Cuisine
+FROM
+    MenuItems
+INNER JOIN
+    Menus
+ON
+    MenuItems.ItemID = Menus.ItemID
+"""
+
+# The execution of the join query stmt
+cursor.execute(join_query)
+
+# Fetching result
+result = cursor.fetchall()
+
+# Getting the columns
+col = cursor.column_names
+
+# Printing columns and result
+print(col)
+for i in result:
+    print(i)
+
