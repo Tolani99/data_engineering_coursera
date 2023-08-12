040dfedd6315ba005ca4a5c5b44889f999d0a1f9
Tolani99 <tolaniadisa999@gmail.com>
2023-08-12 18:07:36 +0100

    Changes

---
+++ database_client/week_3/exercises/tasks/task_2_creating_booking_slots_on_specific_hours.py
@@ -0,0 +1,14 @@
+"""
+Task 2
+Little Lemon’s next task is to find out what time each guest is arriving today. To do this, they need to create three time slots for their guests based on specific booking hours:
+
+Late afternoon: 3pm and 4pm 
+
+Evening: 5pm and 6pm
+
+Night: 7pm and 8pm
+
+
+Help Little Lemon to create these booking slots. Then help them to retrieve the booking ID, guest name, and booking slot for each guest from the database.
+"""
+
