538a814cacc836e40e0cd809822d210edd924942
Tolani99 <tolaniadisa999@gmail.com>
2023-08-12 19:57:41 +0100

    Creating bookign slots on specific hours

--- database_client/week_3/exercises/tasks/task_4_creating_booking_slots_on_specific_hours.py
+++ database_client/week_3/exercises/tasks/task_4_creating_booking_slots_on_specific_hours.py
@@ -11,4 +11,23 @@ Night: 7pm and 8pm
 
 Help Little Lemon to create these booking slots. Then help them to retrieve the booking ID, guest name, and booking slot for each guest from the database.
 """
+from task_1_utilizing_mysql_functions_with_python import cursor
+sql_query = """
+SELECT
+    BookingID,
+    CONCAT(GuestFirstName, " ", GuestLastName) AS GuestName,
 
+CASE
+        WHEN HOUR(BookingSlot) BETWEEN 15 AND 16 THEN "Late afternoon"
+        WHEN HOUR(BookingSlot) BETWEEN 17 AND 18 THEN "Evening"
+        WHEN HOUR(BookingSlot) BETWEEN 19 AND 20 THEN "Night"
+        ELSE "Not a Booking Hour"
+END AS Arrival_slot
+
+FROM Bookings;
+"""
+cursor.execute(sql_query)
+result = cursor.fetchall()
+print(cursor.column_names)
+for i in result:
+    print(i)
