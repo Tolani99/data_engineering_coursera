b937c998d29e47f50190cf8312b37ddf6fc529ee
Tolani99 <tolaniadisa999@gmail.com>
2023-08-13 19:01:00 +0100

    Displaying Specific info

--- database_client/week_3/exercises/exercises_2_working_with_date_and_time/tasks_2_displaying_specific_info.py
+++ database_client/week_3/exercises/exercises_2_working_with_date_and_time/tasks_2_displaying_specific_info.py
@@ -10,7 +10,10 @@ Each guest’s full name,
 And the expected arrival time in hours and minutes of each guest (e.g., 15 hours and 0 mins).
 
 """
+# Importing modules
 from tasks_1_retrieving_number_of_bookings import cursor
+
+# Writing the sql statement
 sql_statement = """
 SELECT
     TableNo, 
@@ -22,13 +25,22 @@ FROM
 ORDER BY 
     BookingSlot;
 """
+
+# Executing with cursor
 cursor.execute(sql_statement)
+
+# Using results to fetch all
 results = cursor.fetchall()
+
+# Priting guest and booking slots
 print("The guests and their booking slots are: ")
+
+# Printing results using for loop
 for row in results:
     TableNo = row[0]
     GuestFullName = row[1]
     Arrival_Time_Hour = row[2]
     Arrival_Time_Minute = row[3]
+
     print("[Table no:] {} >> {} is expected to arrive at: {} hrs and {} mins".format(TableNo, GuestFullName, Arrival_Time_Hour, Arrival_Time_Minute))
 
