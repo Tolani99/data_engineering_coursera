aa2338cc11c85e914456c25d5f4cfa41d39eb1ce
Tolani99 <tolaniadisa999@gmail.com>
2023-08-13 19:06:08 +0100

    changes

---
+++ database_client/week_3/exercises/exercises_2_working_with_date_and_time/tasks_3_making_change_using_python.py
@@ -0,0 +1,13 @@
+"""
+Task 3:
+
+A guest with booking ID 2 and table number 12 wants to change their arrival time by one hour from 7pm to 8pm. Help Little Lemon to make this change using Python in their application. Use the following steps as a guide to complete this task:
+
+Step 1: Target BookingID, TableNo and BookingSlot columns in the Bookings table. 
+
+Step 2: Add one hour in the BookingSlot. 
+
+Step 3: Use the WHERE clause on TableNo and BookingID columns. 
+
+"""
+
