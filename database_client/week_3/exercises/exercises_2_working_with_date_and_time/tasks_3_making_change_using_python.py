7266ef11342c050821eb60049d41de42c19bb97e
Tolani99 <tolaniadisa999@gmail.com>
2023-08-13 19:25:56 +0100

    Making changes using python in a mysql db

--- database_client/week_3/exercises/exercises_2_working_with_date_and_time/tasks_3_making_change_using_python.py
+++ database_client/week_3/exercises/exercises_2_working_with_date_and_time/tasks_3_making_change_using_python.py
@@ -10,4 +10,34 @@ Step 2: Add one hour in the BookingSlot.
 Step 3: Use the WHERE clause on TableNo and BookingID columns. 
 
 """
+# Importing modules
+from tasks_1_retrieving_number_of_bookings import cursor
+import datetime as dt
+
+# Sql statement
+sql_statement = """
+SELECT
+    BookingID,
+    TableNo,
+    BookingSlot
+FROM
+    Bookings;
+"""
+
+# Execute the sql statement
+cursor.execute(sql_statement)
+
+# Printing the results
+results = cursor.fetchall()
+print("Booking time change ALERT!!")
+for row in results:
+    Booking_ID = row[0]
+    Table_No = row[1]
+    Booking_Slot = row[2]
+    if Booking_ID == 2 and Table_No == 12:
+        New_Booking_Slot = Booking_Slot + dt.timedelta(hours = 1)
+        print("Booking ID: {}".format(Booking_ID))
+        print("Table number: {}".format(Table_No))
+        print("Booked slot: {}".format(Booking_Slot))
+        print("New arrival time: {}".format(New_Booking_Slot))
 
