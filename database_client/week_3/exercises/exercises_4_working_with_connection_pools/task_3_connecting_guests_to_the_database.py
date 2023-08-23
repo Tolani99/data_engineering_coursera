fda1044dce448852c9426080010530360dd3b51b
Tolani99 <tolaniadisa999@gmail.com>
2023-08-23 13:08:19 +0100

    Connecting guest to the database

--- database_client/week_3/exercises/exercises_4_working_with_connection_pools/task_3_connecting_guests_to_the_database.py
+++ database_client/week_3/exercises/exercises_4_working_with_connection_pools/task_3_connecting_guests_to_the_database.py
@@ -1,31 +1,17 @@
-from task_1_creating_a_database_connection import pool
+from task_1_creating_a_database_connection import pool, connection
 
 guests = ["Anna", "Marcos", "Diana", "Joakim", "Hiroki"]
 
-# Keep track of the current number of connections
-current_connections = 0
-
-# Iterate through the guests
 for guest in guests:
-    # Get connection
-    with pool.get_connection() as conn:
-        # Check if the connection is established
-        if conn.is_connected():
-            # Create cursor object
-            cursor = conn.cursor()
-            
-            # Perform database operations here
-            # ...
+    try:
+        guest_connected = pool.get_connection()
+        print("[{}] is connected.\n".format(guest))
+    except:
+        print("No more connections are available.")
+        print("Adding new connections in the pool.")
+        # Add the connection into the pool
+        pool.add_connection(cnx = connection)
+        print("A new connection is added in the pool.\n")
 
-            # Print connection status
-            print("{} is connected".format(guest))
-        else:
-            print("It's not connected")
-    
-    # Connection is automatically released after the context block
-    
-    # If the pool doesn't have available connections, add a new connection
-    if current_connections >= pool.pool_size:
-        pool.add_connection()
-        current_connections += 1
-        print("A new connection is added in the pool.")
+        user_connected = pool.get_connection()
+        print("[{}] is connected.\n".format(guest))
