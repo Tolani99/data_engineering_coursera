6baeed857be445a94e6bc6b2d4c0ca810e75a4c5
Tolani99 <tolaniadisa999@gmail.com>
2023-08-25 02:50:50 +0100

    Establishing a connection pool

---
+++ database_client/week_4/final_project/implement_and_query_stored_procedure/exercise_little_lemon_analysis_and_report/task_1_establish_a_connection_pool.py
@@ -0,0 +1,42 @@
+"""
+Task 1:
+Complete the following steps to establish a connection pool:
+
+To create a connection pool, import MySQLConnectionPool class from MySQL Connector/Python.
+
+To find the information on the error, import the Error class from MySQL Connector/Python.
+
+Define your database configurations as a Python dictionary object called dbconfig.
+
+Establish a connection pool [pool_name = pool_b] with two connections.
+
+Implement error handling using a try-except block in case the connection fails.
+"""
+# Import Mysql/Python Connector
+import mysql.connector as connector
+
+# Establish connection
+connection = connector.connect(user = "root", password = "tolamath")
+
+# Create a cursor object
+cursor = connection.cursor()
+
+# Import MySQLConnectionPool
+from mysql.connector.pooling import MySQLConnectionPool, Error
+
+# Define dbconfig
+dbconfig = {"database":"little_lemon_db", 
+            "user":"root",
+            "password":"tolamath"}
+
+# Establishing a connection pool
+try:
+    pool = MySQLConnectionPool(pool_name = "pool_b",
+                               pool_size = 2,
+                                **dbconfig)
+    print("Pool name is {} and Pool size is {}".format(pool.pool_name, pool.pool_size))
+except Error as err:
+    print("Error code is: {}".format(err.errno))
+    print("Error message is: {}".format(err.msg))
+
+
