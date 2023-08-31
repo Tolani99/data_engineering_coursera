ce5e874dd561ba98d0a66f898e9ef84501556d03
Tolani99 <tolaniadisa999@gmail.com>
2023-08-31 23:52:27 +0100

    Little lemon Analysis and report - solved

--- database_client/week_4/final_project/implement_and_query_stored_procedure/exercise_little_lemon_analysis_and_report/task_4_creating_a_stored_procedure.py
+++ database_client/week_4/final_project/implement_and_query_stored_procedure/exercise_little_lemon_analysis_and_report/task_4_creating_a_stored_procedure.py
@@ -1,7 +1,18 @@
 """
+Task 4:
+Create a stored procedure named BasicSalesReport that returns the following statistics:
+
+Total sales
+
+Average sale
+
+Minimum bill paid
+
+Maximum bill paid
 
 """
-CREATE PROCEDURE BasicSalesReport()
+
+sql_stmt = """CREATE PROCEDURE BasicSalesReport()
 BEGIN
     SELECT SUM(TotalAmount) AS TotalSales,
            AVG(TotalAmount) AS AverageSale,
@@ -9,3 +20,4 @@ BEGIN
            MAX(TotalAmount) AS MaximumBillPaid
     FROM Orders;
 END 
+"""
