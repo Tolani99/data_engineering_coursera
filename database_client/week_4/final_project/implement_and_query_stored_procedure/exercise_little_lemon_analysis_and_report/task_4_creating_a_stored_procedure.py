7536606078dfa3c802e3e434f2e54b323fff529e
Tolani99 <tolaniadisa999@gmail.com>
2023-09-15 23:11:13 +0100

    Creating a stored procedure

--- database_client/week_4/final_project/implement_and_query_stored_procedure/exercise_little_lemon_analysis_and_report/task_4_creating_a_stored_procedure.py
+++ database_client/week_4/final_project/implement_and_query_stored_procedure/exercise_little_lemon_analysis_and_report/task_4_creating_a_stored_procedure.py
@@ -9,8 +9,20 @@ Average sale
 Minimum bill paid
 
 Maximum bill paid
-
 """
+
+# Import Python/MySql Connection
+import mysql.connector as connector
+
+# Create a connection
+connection = connector.connect(user = 'root', password = 'tolamath')
+
+from task_1_establish_a_connection_pool.py import pool_b, Error
+
+# Add the connection into the pool
+pool.add_connection(cnx = connection)
+cursor.execute("DROP PROCEDURE IF EXISTS BasicSalesReport;")
+
 
 sql_stmt = """CREATE PROCEDURE BasicSalesReport()
 BEGIN
@@ -21,3 +33,26 @@ BEGIN
     FROM Orders;
 END 
 """
+
+# Execute the query
+cursor.execute(sql_stmt)
+
+#*******************************#
+
+# Call the stored procedure with its name
+cursor.callproc("BasicSalesReport")
+
+# Retrieve records in "dataset"
+results = next(cursor.stored_results())
+dataset = results.fetchall()
+
+# Retrieve column names using list comprehension in a 'for' loop
+for column_id in cursor.stored_results():
+    columns = [column[0] for column in column_id.description]
+
+# Print column names
+print(columns)
+
+# Print data
+for data in dataset:
+    print(data)
